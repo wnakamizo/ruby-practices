e4baf86b47cf34c894c5bfce0535520397737e07
wnakamizo <w.nakamizo@genbasupport.com>
2026-08-10 10:13:24 +0900

    style: add spaces around modulo operators

--- 01.fizzbuzz/fizzbuzz_wn.rb
+++ 01.fizzbuzz/fizzbuzz_wn.rb
@@ -1,11 +1,11 @@
 #!/usr/bin/env ruby
 
 (1..20).each do |i|
-  if (i%3).zero? && (i%5).zero?
+  if (i % 3).zero? && (i % 5).zero?
     puts "FizzBuzz"
-  elsif (i%3).zero?
+  elsif (i % 3).zero?
     puts "Fizz"
-  elsif (i%5).zero?
+  elsif (i % 5).zero?
     puts "Buzz"
   else
     puts i
