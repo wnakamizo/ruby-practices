ce88630d49de35417fdce2d12cb505e8553c4bd4
wnakamizo <w.nakamizo@genbasupport.com>
2026-08-11 10:41:43 +0900

    chore: move calendar_wn.rb to 02.calendar

---
+++ 02.calendar/calendar_wn.rb
@@ -0,0 +1,36 @@
+#!/usr/bin/env ruby
+require "date"
+require "optparse"
+today = Date.today
+month = today.month
+year = today.year
+
+opt = OptionParser.new
+opt.on('-m MONTH') { |m| month = m.to_i }
+opt.on('-y YEAR') { |y| year = y.to_i }
+opt.parse!(ARGV)
+
+sub_header = "Su Mo Tu We Th Fr Sa"
+
+first_date = Date.new(year, month, 1)
+last_date = Date.new(year, month, -1)
+month_name = first_date.strftime("%B")
+header = "#{month_name} #{year}".center(sub_header.size)
+
+three_spaces = "   "
+first_week_indent = three_spaces*(first_date.strftime("%w").to_i)
+weeks = (first_date..last_date).slice_before(&:sunday?)
+body = weeks.map.with_index do |week, index|
+  formatted_week = week.map { _1.strftime("%e")}.join(" ")
+  if index.zero?
+    first_week_indent + formatted_week
+  else
+    formatted_week
+  end
+end
+
+puts [
+  header,
+  sub_header,
+  body
+]
