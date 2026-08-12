cebbd2ca686575c9216425d4014e17d18d8ef800
wnakamizo <w.nakamizo@genbasupport.com>
2026-08-12 11:32:07 +0900

    Rename variables for readability

--- 03.bowling/bowling_wn.rb
+++ 03.bowling/bowling_wn.rb
@@ -1,27 +1,27 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-input = ARGV[0].split(',')
-converted_input = input.map { |shot| shot == 'X' ? 10 : shot.to_i }
-frame_number = 1
+shots = ARGV[0].split(',')
+scores = shots.map { |shot| shot == 'X' ? 10 : shot.to_i }
+current_frame = 1
 first_shot = true
-bonus_score_strike = 0
-bonus_score_spare = 0
+strike_bonus = 0
+spare_bonus = 0
 base_score = 0
-converted_input.each_with_index do |score, i|
+scores.each_with_index do |score, i|
   base_score += score
   if score == 10 && first_shot
-    bonus_score_strike += converted_input[i + 1] + converted_input[i + 2]
-    frame_number += 1
+    strike_bonus += scores[i + 1] + scores[i + 2]
+    current_frame += 1
   elsif first_shot
-    bonus_score_spare += converted_input[i + 2] if score + converted_input[i + 1] == 10
+    spare_bonus += scores[i + 2] if score + scores[i + 1] == 10
     first_shot = false
   else
-    frame_number += 1
+    current_frame += 1
     first_shot = true
   end
-  break if frame_number == 11
+  break if current_frame == 11
 end
 
-total_score = base_score + bonus_score_spare + bonus_score_strike
+total_score = base_score + spare_bonus + strike_bonus
 puts total_score
