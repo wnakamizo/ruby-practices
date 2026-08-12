600dd2b1ad90a5e52daf930d4801a8fae759c21c
wnakamizo <w.nakamizo@genbasupport.com>
2026-08-12 11:49:43 +0900

    Merge base_score, strike_bonus, and spare_bonus into total_score

--- 03.bowling/bowling_wn.rb
+++ 03.bowling/bowling_wn.rb
@@ -5,16 +5,14 @@ shots = ARGV[0].split(',')
 scores = shots.map { |shot| shot == 'X' ? 10 : shot.to_i }
 current_frame = 1
 first_shot = true
-strike_bonus = 0
-spare_bonus = 0
-base_score = 0
+total_score = 0
 scores.each_with_index do |score, i|
-  base_score += score
+  total_score += score
   if score == 10 && first_shot
-    strike_bonus += scores[i + 1] + scores[i + 2]
+    total_score += scores[i + 1] + scores[i + 2]
     current_frame += 1
   elsif first_shot
-    spare_bonus += scores[i + 2] if score + scores[i + 1] == 10
+    total_score += scores[i + 2] if score + scores[i + 1] == 10
     first_shot = false
   else
     current_frame += 1
@@ -23,5 +21,4 @@ scores.each_with_index do |score, i|
   break if current_frame == 11
 end
 
-total_score = base_score + spare_bonus + strike_bonus
 puts total_score
