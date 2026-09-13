41f088a7ef58047ebc176b7e1ec4308d238071d6
wnakamizo <w.nakamizo@genbasupport.com>
2026-09-14 01:17:34 +0900

    Replace individual shot attributes with a shots array in Frame

--- 06.bowling_object/lib/frame.rb
+++ 06.bowling_object/lib/frame.rb
@@ -1,20 +1,18 @@
 # frozen_string_literal: true
 
 class Frame
-  attr_reader :frame_num, :first_shot, :second_shot, :third_shot
+  attr_reader :frame_num, :shots
 
   def initialize(frame_num, first_shot, second_shot = nil, third_shot = nil)
     @frame_num = frame_num
-    @first_shot = first_shot
-    @second_shot = second_shot
-    @third_shot = third_shot
+    @shots = [first_shot, second_shot, third_shot].compact
   end
 
-  def score(shots)
+  def score(all_shots)
     additive_score = if strike? && !last_frame?
-                       first_shot.next_n_shots(shots, 2).sum(&:pin_count)
+                       shots[0].next_n_shots(all_shots, 2).sum(&:pin_count)
                      elsif spare? && !last_frame?
-                       second_shot.next_n_shots(shots, 1)[0].pin_count
+                       shots[1].next_n_shots(all_shots, 1)[0].pin_count
                      else
                        0
                      end
@@ -22,12 +20,11 @@ class Frame
   end
 
   def pins_knocked_down
-    shots = [first_shot, second_shot, third_shot]
-    shots.compact.sum(&:pin_count)
+    shots.sum(&:pin_count)
   end
 
   def strike?
-    first_shot.strike?
+    shots[0].strike?
   end
 
   def spare?
